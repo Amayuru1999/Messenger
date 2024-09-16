39188328667a2d238ded5ba333a961373f3a028e
Amayuru1999 <amarasingheau@gmail.com>
2024-09-16 20:55:40 +0530

    feat: added react-hook-form with functionalities

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,6 @@
 'use client'
 import {useCallback, useState} from "react";
+import {FieldValue, FieldValues, SubmitHandler, useForm} from "react-hook-form";
 
 type Variant = 'LOGIN' | 'REGISTER'
 
@@ -15,6 +16,34 @@ const AuthForm = () =>{
             setVariant('LOGIN')
         }
     },[variant])
+
+    const {
+        register,
+        handleSubmit,
+        formState:{errors},
+    } = useForm<FieldValues>({
+        defaultValues:{
+            name:'',
+            email:'',
+            password:'',
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true)
+        if(variant === 'LOGIN') {
+            console.log('Logging in with', data)
+        }
+        if(variant === 'REGISTER') {
+            console.log('Registering with', data)
+        }
+    }
+
+    const socialAction = (action:string) =>{
+        setIsLoading(true)
+
+        //NextAuth social sign in
+    }
 
     return(
         <div>
